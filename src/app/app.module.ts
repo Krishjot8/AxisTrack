724b39b9076d85bebe819cec9a1d99f26283499d
Krishjot8 <krishjot8@yahoo.com>
2025-09-23 15:46:00 -0500

    yes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import {SidebarComponent } from './components/sidebar/sidebar.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { SidebarModule } from 'primeng/sidebar';
 import { ButtonModule } from 'primeng/button';
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    DashboardComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    SidebarModule,
-ButtonModule
-
-
-
-  ],
-  providers: [
-
-
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, DashboardComponent, SidebarComponent],
+  imports: [BrowserModule, AppRoutingModule, SidebarModule, ButtonModule],
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
